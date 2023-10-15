b7eee1c9b66d66a5b1b69329a8028ee94eb79caa
Sergio Rosa <scarlosjazz@gmail.com>
2023-10-15 17:07:30 -0400

    deliveryApp - urls-Product

---
+++ produto/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name='home'),
+     path("categoria/<int:id>", views.categorias, name='categoria'),
+     path("produto/<int:id>", views.produto, name='produto'),
+     path("add_carrinho", views.add_carrinho, name='add_carrinho'),
+]
