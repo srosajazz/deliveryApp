11334d3a051c8cba9e0fe6fe021a73c9fbe63bdc
Sergio Rosa <scarlosjazz@gmail.com>
2023-10-20 20:15:16 -0400

    cupom

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import ItemPedido, Pedido
+from .models import ItemPedido, Pedido,CupomDesconto
 from django.http import HttpResponse
 # Register your models here.
+
+@admin.register(CupomDesconto)
+class CupomDescontoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'desconto', 'ativo')
+    readonly_fields=('usos',)
 
 class itemPedidoInline(admin.TabularInline):
     readonly_fields = ('produto', 'quantidade', 'preco', 'descricao', 'adicionais',)
@@ -15,6 +20,6 @@ class PedidoAdmin(admin.ModelAdmin):
     ]
     list_display = ('usuario', 'total', 'data', 'entregue')
     search_fields = ('entregue',)
-    readonly_fields = ('usuario', 'total', 'troco', 'pagamento', 'ponto_referencia', 'data', 'cep', 'rua', 'numero', 'bairro', 'telefone')
+    readonly_fields = ('usuario', 'total', 'troco','pagamento', 'ponto_referencia', 'data', 'cep', 'rua', 'numero', 'bairro', 'telefone')
     list_filter = ('entregue',)
 admin.site.register(Pedido, PedidoAdmin)
